1f8167bf5996f7261427d6a76d111a6ea37dc52b
Xwaffle <chasecool56@gmail.com>
2022-08-23 22:38:17 -0400

    Include fontawesome in HEAD

--- pages/index.js
+++ pages/index.js
@@ -469,6 +469,10 @@ export default function Home() {
       <Head>
         <title>Chase Myers</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
+          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" 
+          crossorigin="anonymous" referrerpolicy="no-referrer" 
+        />
       </Head>
       <MainContent />
       <footer className={styles.footer} style={{textAlign: "left"}}>
