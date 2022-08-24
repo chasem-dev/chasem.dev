90b65adc24c882fe1e9d71d37e1d2e900a160654
Xwaffle <chasecool56@gmail.com>
2022-08-23 22:42:47 -0400

    Include fontawesome locally

--- pages/index.js
+++ pages/index.js
@@ -469,10 +469,7 @@ export default function Home() {
       <Head>
         <title>Chase Myers</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" 
-          integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" 
-          crossorigin="anonymous" referrerpolicy="no-referrer" 
-        />
+        <script src='/js/fontawesome.min.js' />
       </Head>
       <MainContent />
       <footer className={styles.footer} style={{textAlign: "left"}}>
